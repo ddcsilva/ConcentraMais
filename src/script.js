d2f15d98ecb544100f5e08d1fba5ff920446ef9b
danilo.silva <danilo.silva@msn.com>
2024-06-10 01:53:37 -0300

    Deixando as frases em escopo global

--- src/script.js
+++ src/script.js
@@ -6,6 +6,12 @@ const inputMusica = document.querySelector('#alternancia-musica');
 const musica = new Audio('sons/lofi-study.mp3');
 musica.loop = true;
 
+const frases = {
+    'foco': 'Aprimore sua eficiência,<br> <strong>concentre-se no essencial</strong>.',
+    'descanso-curto': 'Aproveite um breve descanso,<br> para <strong>recarregar suas energias</strong>.',
+    'descanso-longo': 'Faça uma pausa prolongada,<br> e volte com <strong>ainda mais foco</strong>.'
+};
+
 inputMusica.addEventListener('change', () => {
     if (inputMusica.checked) {
         musica.play();
@@ -24,13 +30,6 @@ botoes.forEach(botao => {
 
 function alterarModo(modo) {
     html.setAttribute('data-modo', modo);
-
-    const frases = {
-        'foco': 'Aprimore sua eficiência,<br> <strong>concentre-se no essencial</strong>.',
-        'descanso-curto': 'Aproveite um breve descanso,<br> para <strong>recarregar suas energias</strong>.',
-        'descanso-longo': 'Faça uma pausa prolongada,<br> e volte com <strong>ainda mais foco</strong>.'
-    };
-
     frase.innerHTML = frases[modo] || '';
 }
 
