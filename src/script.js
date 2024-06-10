2b61856947ee3a461e2473543ddc80c169b10ae7
danilo.silva <danilo.silva@msn.com>
2024-06-10 01:52:36 -0300

    Refatoração

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
 const html = document.querySelector('html');
-const botaoFoco = document.querySelector('.botao-foco');
-const botaoDescansoCurto = document.querySelector('.botao-descanso-curto');
-const botaoDescansoLongo = document.querySelector('.botao-descanso-longo');
 const frase = document.querySelector('.frase');
 const botoes = document.querySelectorAll('.botao');
 const inputMusica = document.querySelector('#alternancia-musica');
@@ -17,42 +14,27 @@ inputMusica.addEventListener('change', () => {
     }
 });
 
-botaoFoco.addEventListener('click', () => {
-    alterarModo('foco');
-    botaoFoco.classList.add('selecionado');
-});
-
-botaoDescansoCurto.addEventListener('click', () => {
-    alterarModo('descanso-curto');
-    botaoDescansoCurto.classList.add('selecionado');
-});
-
-botaoDescansoLongo.addEventListener('click', () => {
-    alterarModo('descanso-longo');
-    botaoDescansoLongo.classList.add('selecionado');
+botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const modo = botao.getAttribute('data-modo');
+        alterarModo(modo);
+        atualizarSelecao(botao);
+    });
 });
 
 function alterarModo(modo) {
-    botoes.forEach(botao => botao.classList.remove('selecionado'));
     html.setAttribute('data-modo', modo);
 
-    switch (modo) {
-        case 'foco':
-            frase.innerHTML = `
-                Aprimore sua eficiência,<br> <strong>concentre-se no essencial</strong>.
-            `;
-            break;
-        case 'descanso-curto':
-            frase.innerHTML = `
-                Aproveite um breve descanso,<br> para <strong>recarregar suas energias</strong>.
-            `;
-            break;
-        case 'descanso-longo':
-            frase.innerHTML = `
-                Faça uma pausa prolongada,<br> e volte com <strong>ainda mais foco</strong>.
-            `;
-            break;
-        default:
-            break;
-    }
+    const frases = {
+        'foco': 'Aprimore sua eficiência,<br> <strong>concentre-se no essencial</strong>.',
+        'descanso-curto': 'Aproveite um breve descanso,<br> para <strong>recarregar suas energias</strong>.',
+        'descanso-longo': 'Faça uma pausa prolongada,<br> e volte com <strong>ainda mais foco</strong>.'
+    };
+
+    frase.innerHTML = frases[modo] || '';
+}
+
+function atualizarSelecao(botaoSelecionado) {
+    botoes.forEach(botao => botao.classList.remove('selecionado'));
+    botaoSelecionado.classList.add('selecionado');
 }
